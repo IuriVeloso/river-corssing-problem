3c51f0ce2d030bc1514dd843d181945b1b1e8e80
IuriVeloso <iuriv.felix@gmail.com>
2022-11-26 15:45:42 -0300

    add sequential logic

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <pthread.h>
 #include <time.h>
 
-#define MAX_BOAT_VISITORS 8
+#define MAX_BOAT_VISITORS 20
 
 #define ARRIVED_HACKER 0
 #define ARRIVED_SERF 1
@@ -14,30 +14,67 @@ int totalPassengers = 0;
 
 time_t t;
 
+pthread_cond_t boardOpen = PTHREAD_COND_INITIALIZER;
 pthread_cond_t boarding = PTHREAD_COND_INITIALIZER;
-pthread_cond_t passengerBoarded = PTHREAD_COND_INITIALIZER;
+pthread_cond_t boardFull = PTHREAD_COND_INITIALIZER;
 
 int isBoardOpen = 0;
+int isBoarding = 0;
+int isBoardFull = 0;
 
 pthread_mutex_t mutexCountPassengers = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexBoardFull = PTHREAD_MUTEX_INITIALIZER;
+
 pthread_mutex_t mutexCanBoard = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t mutexSomeoneBoarding = PTHREAD_MUTEX_INITIALIZER;
+
 
 void *serf(void *args) {
     printf("\n\n1- Serf aguarda para embarcar %d!\n", *(int *)args);
 
     pthread_mutex_lock(&mutexCanBoard);
     while(isBoardOpen == 0){
-        pthread_cond_wait(&boarding, &mutexCanBoard);
+        pthread_cond_wait(&boardOpen, &mutexCanBoard);
     }
     pthread_mutex_unlock(&mutexCanBoard);
-    printf("\n2- Serf esta embarcando %d\n", *(int *)args);
 
+    printf("\n2- Serf na fila para embarcar %d!\n", *(int *)args);
+
+    pthread_mutex_lock(&mutexSomeoneBoarding);
+    while(isBoarding == 1){
+        pthread_cond_wait(&boarding, &mutexSomeoneBoarding);
+    }
+    isBoarding = 1;
+    pthread_mutex_unlock(&mutexSomeoneBoarding);
+
+    printf("\n3- Serf aguarda seu lugar no barco %d!\n", *(int *)args);
+
+    pthread_mutex_lock(&mutexBoardFull);
+    while(isBoardFull == 1){
+        pthread_cond_wait(&boardFull, &mutexBoardFull);
+    }
+    pthread_mutex_unlock(&mutexBoardFull);
+
+    printf("\n4 -Serf esta embarcando %d\n", *(int *)args);
+    
     pthread_mutex_lock(&mutexCountPassengers);
     passengersOnBoard ++;
-    pthread_cond_signal(&passengerBoarded);
-
+    if(passengersOnBoard == 4){
+        pthread_mutex_lock(&mutexBoardFull);
+        isBoardFull = 1;
+        pthread_cond_signal(&boardFull);
+        pthread_mutex_unlock(&mutexBoardFull);
+    }
     pthread_mutex_unlock(&mutexCountPassengers);
-    printf("\n3- Vai o serf %d\n\n", *(int *)args);
+
+    printf("\n5 -Vai o serf %d\n\n", *(int *)args);
+
+    pthread_mutex_lock(&mutexSomeoneBoarding);
+    isBoarding = 0;
+    pthread_cond_signal(&boarding);
+    pthread_mutex_unlock(&mutexSomeoneBoarding);
+
+    printf("\n6 -Liberou a vaga %d\n\n", *(int *)args);
 
     pthread_exit(NULL);
 }
@@ -47,18 +84,47 @@ void *hacker(void *args) {
 
     pthread_mutex_lock(&mutexCanBoard);
     while(isBoardOpen == 0){
-        pthread_cond_wait(&boarding, &mutexCanBoard);
+        pthread_cond_wait(&boardOpen, &mutexCanBoard);
     }
     pthread_mutex_unlock(&mutexCanBoard);
 
-    printf("\n2 -Hacker esta embarcando %d\n", *(int *)args);
+    printf("\n2- Hacker na fila para embarcar %d!\n", *(int *)args);
+
+    pthread_mutex_lock(&mutexSomeoneBoarding);
+    while(isBoarding == 1){
+        pthread_cond_wait(&boarding, &mutexSomeoneBoarding);
+    }
+    isBoarding = 1;
+    pthread_mutex_unlock(&mutexSomeoneBoarding);
+
+    printf("\n3- Hacker aguarda seu lugar no barco %d!\n", *(int *)args);
+
+    pthread_mutex_lock(&mutexBoardFull);
+    while(isBoardFull == 1){
+        pthread_cond_wait(&boardFull, &mutexBoardFull);
+    }
+    pthread_mutex_unlock(&mutexBoardFull);
+
+    printf("\n4 -Hacker esta embarcando %d\n", *(int *)args);
     
     pthread_mutex_lock(&mutexCountPassengers);
     passengersOnBoard ++;
-    pthread_cond_signal(&passengerBoarded);
-
+    if(passengersOnBoard == 4){
+        pthread_mutex_lock(&mutexBoardFull);
+        isBoardFull = 1;
+        pthread_cond_signal(&boardFull);
+        pthread_mutex_unlock(&mutexBoardFull);
+    }
     pthread_mutex_unlock(&mutexCountPassengers);
-    printf("\n3 -Vai o hacker %d\n\n", *(int *)args);
+
+    printf("\n5 -Vai o hacker %d\n\n", *(int *)args);
+
+    pthread_mutex_lock(&mutexSomeoneBoarding);
+    isBoarding = 0;
+    pthread_cond_signal(&boarding);
+    pthread_mutex_unlock(&mutexSomeoneBoarding);
+
+    printf("\n6 -Liberou a vaga %d\n\n", *(int *)args);
 
     pthread_exit(NULL);
 }
@@ -71,25 +137,27 @@ void *barco(void *args) {
         
         pthread_mutex_lock(&mutexCanBoard);
         isBoardOpen = 1;
-
-        pthread_cond_signal(&boarding);
+        pthread_cond_signal(&boardOpen);
         pthread_mutex_unlock(&mutexCanBoard);
 
-        
-        pthread_mutex_lock(&mutexCountPassengers);
-        while(passengersOnBoard < 4){
-            pthread_cond_wait(&passengerBoarded, &mutexCountPassengers);
-        }
-        printf("\n\nPartiu um barco\n\n");
-        pthread_mutex_unlock(&mutexCountPassengers);
+        pthread_mutex_lock(&mutexBoardFull);
+        isBoardFull = 0;
+        passengersOnBoard = 0;
+        pthread_cond_signal(&boardFull);
+        pthread_mutex_unlock(&mutexBoardFull);
 
+        
+        pthread_mutex_lock(&mutexBoardFull);
+        while(isBoardFull == 0){
+            pthread_cond_wait(&boardFull, &mutexBoardFull);
+        }
+        pthread_mutex_unlock(&mutexBoardFull);
+
+        printf("\n\nPartiu um barco com %d\n\n", passengersOnBoard);
+ 
         pthread_mutex_lock(&mutexCanBoard);
         isBoardOpen = 0;
         pthread_mutex_unlock(&mutexCanBoard);
-
-        pthread_mutex_lock(&mutexCountPassengers);
-        passengersOnBoard = 0;
-        pthread_mutex_unlock(&mutexCountPassengers);
 
         totalPassengers += 4;
     }
